4938d253961a5cd903a1350bb8e85a7873fda450
Tiarious Preston <tpreston06@yahoo.com>
2022-08-02 17:42:31 -0400

    Connected database can see JSON in browser

--- server.js
+++ server.js
@@ -8,19 +8,21 @@ app.set('view engine', 'jsx') //setting up our HTML template
 app.engine('jsx', require('express-react-views').createEngine()) //Initializing our view engine
 
 //route
-app.get('/', (req, res) => {
-    res.send('Welcome to Pokemon App!');
-});
+
+// app.get('/pokemon/:id', (req, res) => {
+//      res.render('Show', {pokemon: pokemon[req.params.id]});
+//  });
+
+//  app.get('/pokemon/:id', function(req, res) {
+//     res.send(req.params.id)
+// });
+
 app.get('/pokemon', (req, res) => {
     res.render('Index', {pokemon: pokemon});
 });
 
-app.get('/pokemon/:id', (req, res) => {
-     res.render('Show', {pokemon: pokemon[req.params.id]});
- });
-
- app.get('/pokemon/:id', function(req, res) {
-    res.send(req.params.id)
+app.get('/', (req, res) => {
+    res.send('Welcome to Pokemon App!');
 });
 
 
